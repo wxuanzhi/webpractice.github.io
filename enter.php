57a00364f38729f8744e7012328f886aef4e2baa
wxuanzhi <43362992+wxuanzhi@users.noreply.github.com>
2020-03-26 10:39:29 +0800

    Update enter.php

--- enter.php
+++ enter.php
@@ -1,13 +1,13 @@
 <?php
 
  	$username = $_POST['username'];
- 	$password = $_POST['password'];
+ 	$password = $_POST['password'];//獲取表單資料
 
 include ('connect.php');
 $sql = "insert into test (username , password) values ('$username' , '$password')";
-$result = $conn -> query($sql);
+$result = $conn -> query($sql);//執行sql
 if(!$result){
-	die('ERROR:' . mysql_error());
+	die('ERROR:' . mysql_error());//如果sql執行失敗輸出錯誤
 }else{
 	echo "註冊成功";
 }
