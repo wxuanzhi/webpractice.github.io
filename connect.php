7bdfcc0ca2aeef9bf50d82ff87f9f052faf9edb0
wxuanzhi <43362992+wxuanzhi@users.noreply.github.com>
2020-03-26 10:46:56 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,11 +3,11 @@
  	$username = $_POST['username'];
  	$password = $_POST['password'];
 
-$db = 'mysql';
-$dbhost = 'localhost';
-$dbname = 'test';
-$dbuser = 'root';
-$dbpass = '';
+$db = 'mysql';//資料庫類型
+$dbhost = 'localhost';//資料庫主機名
+$dbname = 'test';//資料庫
+$dbuser = 'root';//資料庫用戶名
+$dbpass = '';//對應的密碼
 $conn = new mysqli("$dbhost" , "$dbuser" , "$dbpass" , "$dbname");
 $result = $conn -> query("select * from test where username = '$username' and password = '$password'");
 
